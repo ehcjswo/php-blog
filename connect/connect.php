d7ed207df5b72922e19602d26d9868fc12e404cc
KimDoHyeon <ehcjswo1@gmail.com>
2023-07-25 16:37:06 +0900

    07-25 php2 delete

--- connect/connect.php
+++ connect/connect.php
@@ -5,6 +5,21 @@
     $db = "phpclass";
     $connect = new mysqli($host,$user,$pw,$db);
     $connect -> set_charset("utf-8");
+    if(mysqli_connect_errno()){
+        echo "Database Connect false";
+    } else {
+        echo "Database Connect True";
+    }
+?>
+<?php
+    // phpinfo()
+    // $host = "localhost";
+    // $user = "ehcjswo123";
+    // $pw = "zosdb12!";
+    // $db = "ehcjswo123";
+    // $connect = new mysqli($host, $user, $pw, $db);
+    // $connect -> set_charset("utf-8");
+
     // if(mysqli_connect_errno()){
     //     echo "Database Connect false";
     // } else {
--- php2/include/infoLogo.php
+++
@@ -1,3 +0,0 @@
-<div class="info__logo">
-    <h1>Abide By Beauty</h1>
-</div>
